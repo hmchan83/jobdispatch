14000147045c1afb19c80a46311dc485398de012
joseph1125@gmail.com <joseph1125@gmail.com@717cf2e0-59f1-bb26-a36b-0e1b57ee16fe>
2014-07-06 02:38:50 +0000

    added welcome.jsp to redirect user to the index page modified some functions of pageControllers added images for 404 page added more classes on helper.css

--- trunk/src/java/beanController/BeanController.java
+++ trunk/src/java/beanController/BeanController.java
@@ -1,44 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package beanController;
 
 import java.sql.*;
 import bean.*;
-
 
 /**
  *
  * @author Marcus
  */
 public class BeanController {
-    
-    private String url="jdbc:sqlserver://w2ksa.cs.cityu.edu.hk:1433;databaseName=aiad901_db;";
-    private String login="aiad018";
-    private String passwd="aiad018";
+
+    private String url = "jdbc:sqlserver://w2ksa.cs.cityu.edu.hk:1433;databaseName=aiad901_db;";
+    private String login = "aiad018";
+    private String passwd = "aiad018";
     protected Connection con;
     private PreparedStatement pStmt;
 
     public BeanController() {
         try {
-            this.con = DriverManager.getConnection(url,login,passwd);
+            this.con = DriverManager.getConnection(url, login, passwd);
         } catch (SQLException ex) {
             error(ex);
         }
     }
-    
-    public void close(){
+
+    public void close() {
         try {
             con.close();
         } catch (SQLException ex) {
             error(ex);
         }
     }
-    
-    public void error(Exception ex){
+
+    public void error(Exception ex) {
         System.out.println(ex.getMessage());
     }
 
@@ -47,20 +40,29 @@ public class BeanController {
     }
 
     public void setpStmt(String pStmt) {
-        try{
-        this.pStmt = con.prepareStatement(pStmt);
-        }catch (SQLException ex) {
+        try {
+            this.pStmt = con.prepareStatement(pStmt);
+        } catch (SQLException ex) {
             error(ex);
         }
     }
-    public ResultSet exculute(){
-        try{
-            ResultSet rs=this.pStmt.executeQuery();
+
+    public ResultSet execute() {
+        try {
+            ResultSet rs = this.pStmt.executeQuery();
             return rs;
-        }catch (SQLException ex) {
+        } catch (SQLException ex) {
             error(ex);
             return null;
         }
     }
     
+    public void executeUpdate(){
+        try{
+            this.pStmt.executeUpdate();
+        } catch (SQLException ex){
+            error(ex);
+        }
+    }
+
 }
--- trunk/src/java/beanController/TaskController.java
+++ trunk/src/java/beanController/TaskController.java
@@ -1,54 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package beanController;
-
 
 import bean.*;
 import java.sql.*;
 
 /**
  *
- * @author Marcus
- * For controlling a single Task, For adding a new Task, please use TaskListcontroller
+ * @author Marcus For controlling a single Task, For adding a new Task, please
+ * use TaskListcontroller
  */
-public class TaskController extends BeanController{
+public class TaskController extends BeanController {
 
     public TaskController() {
         super();
-    }    
-    
-    public Task getTaskDetail(int taskID){
+    }
+
+    public Task getTaskDetail(int taskID) {
         try {
-            Task t= new Task();
+            Task t = new Task();
             super.setpStmt("SELECT TaskID, TaskName, TypeID, StatusID, PriorityID, TaskDescription, AssigneeID, ReporterID FROM Task Where TaskID = ?");
             super.getpStmt().setInt(1, taskID);
-            ResultSet rs=super.getpStmt().executeQuery();
+            ResultSet rs = super.execute();
             TaskTypeController taskTypeCon = new TaskTypeController();
             StatusController statusCon = new StatusController();
             PriorityController priorityCon = new PriorityController();
-            while(rs.next()){
+            while (rs.next()) {
                 t.setTaskID(rs.getInt(1)); //TaskID
                 t.setTaskName(rs.getString(2)); //TaskName
                 t.setTaskType(taskTypeCon.getTaskType(rs.getInt(3))); //TaskType
                 t.setStatus(statusCon.getStatus(rs.getInt(4))); //Status
                 t.setPriority(priorityCon.getPriority(rs.getInt(5)));
                 t.setDescription(rs.getString(6));
-                
             }
             return t;
         } catch (SQLException ex) {
             return null;
         }
     }
-    
-    public void modifyTask(int taskID, Task newInfo){//Update a Task
-        
+
+    public void modifyTask(int taskID, Task newInfo) {//Update a Task
+
     }
-    
+
     //Lazy
-    public Task get(int ID){return getTaskDetail(ID);}
+    public Task get(int ID) {
+        return getTaskDetail(ID);
+    }
 }
