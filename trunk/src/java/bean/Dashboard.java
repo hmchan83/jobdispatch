8e5a0b69d4c69a8adfd429c54b68b4eb39c151e6
joseph1125@gmail.com <joseph1125@gmail.com@717cf2e0-59f1-bb26-a36b-0e1b57ee16fe>
2014-07-12 10:16:00 +0000

    reuse another data type for dashboard.java rewrite DashboardController.java

--- trunk/src/java/bean/Dashboard.java
+++ trunk/src/java/bean/Dashboard.java
@@ -1,6 +1,6 @@
 package bean;
 
-import java.util.TreeMap;
+import java.util.HashMap;
 
 /**
  *
@@ -9,15 +9,27 @@ import java.util.TreeMap;
 public class Dashboard {
     private int total_resolved_tasks;
     private int total_unresolved_tasks;
-    private TreeMap high_workload_ppl;
-    private TreeMap low_workload_ppl;
-    private TreeMap urgent_tasks;
-    private TreeMap resolved_priority;
-    private TreeMap unresolved_priority;
-    private TreeMap resolved_type;
-    private TreeMap unresolved_type;
-    private TreeMap productive_ppl;
-    private TreeMap non_productive_ppl;
+    private HashMap high_workload_ppl;
+    private HashMap low_workload_ppl;
+    private HashMap urgent_tasks;
+    private HashMap resolved_priority;
+    private HashMap unresolved_priority;
+    private HashMap resolved_type;
+    private HashMap unresolved_type;
+    private HashMap productive_ppl;
+    private HashMap non_productive_ppl;
+    
+    public Dashboard(){
+        high_workload_ppl = new HashMap<Staff, Integer>();
+        low_workload_ppl = new HashMap<Staff, Integer>();
+        urgent_tasks = new HashMap<Task, Long>();
+        resolved_priority = new HashMap<Priority, Integer>();
+        unresolved_priority = new HashMap<Priority, Integer>();
+        resolved_type = new HashMap<TaskType, Integer>();
+        unresolved_type = new HashMap<TaskType, Integer>();
+        productive_ppl = new HashMap<Staff, Integer>();
+        non_productive_ppl = new HashMap<Staff, Integer>();
+    }
 
     public int getTotal_resolved_tasks() {
         return total_resolved_tasks;
@@ -35,75 +47,75 @@ public class Dashboard {
         this.total_unresolved_tasks = total_unresolved_tasks;
     }
     
-    public TreeMap getHigh_workload_ppl() {
+    public HashMap getHigh_workload_ppl() {
         return high_workload_ppl;
     }
 
-    public void setHigh_workload_ppl(TreeMap high_workload_ppl) {
+    public void setHigh_workload_ppl(HashMap high_workload_ppl) {
         this.high_workload_ppl = high_workload_ppl;
     }
 
-    public TreeMap getLow_workload_ppl() {
+    public HashMap getLow_workload_ppl() {
         return low_workload_ppl;
     }
 
-    public void setLow_workload_ppl(TreeMap low_workload_ppl) {
+    public void setLow_workload_ppl(HashMap low_workload_ppl) {
         this.low_workload_ppl = low_workload_ppl;
     }
 
-    public TreeMap getUrgent_tasks() {
+    public HashMap getUrgent_tasks() {
         return urgent_tasks;
     }
 
-    public void setUrgent_tasks(TreeMap urgent_tasks) {
+    public void setUrgent_tasks(HashMap urgent_tasks) {
         this.urgent_tasks = urgent_tasks;
     }
 
-    public TreeMap getResolved_priority() {
+    public HashMap getResolved_priority() {
         return resolved_priority;
     }
 
-    public void setResolved_priority(TreeMap resolved_priority) {
+    public void setResolved_priority(HashMap resolved_priority) {
         this.resolved_priority = resolved_priority;
     }
 
-    public TreeMap getUnresolved_priority() {
+    public HashMap getUnresolved_priority() {
         return unresolved_priority;
     }
 
-    public void setUnresolved_priority(TreeMap unresolved_priority) {
+    public void setUnresolved_priority(HashMap unresolved_priority) {
         this.unresolved_priority = unresolved_priority;
     }
 
-    public TreeMap getResolved_type() {
+    public HashMap getResolved_type() {
         return resolved_type;
     }
 
-    public void setResolved_type(TreeMap resolved_type) {
+    public void setResolved_type(HashMap resolved_type) {
         this.resolved_type = resolved_type;
     }
 
-    public TreeMap getUnresolved_type() {
+    public HashMap getUnresolved_type() {
         return unresolved_type;
     }
 
-    public void setUnresolved_type(TreeMap unresolved_type) {
+    public void setUnresolved_type(HashMap unresolved_type) {
         this.unresolved_type = unresolved_type;
     }
 
-    public TreeMap getProductive_ppl() {
+    public HashMap getProductive_ppl() {
         return productive_ppl;
     }
 
-    public void setProductive_ppl(TreeMap productive_ppl) {
+    public void setProductive_ppl(HashMap productive_ppl) {
         this.productive_ppl = productive_ppl;
     }
 
-    public TreeMap getNon_productive_ppl() {
+    public HashMap getNon_productive_ppl() {
         return non_productive_ppl;
     }
 
-    public void setNon_productive_ppl(TreeMap non_productive_ppl) {
+    public void setNon_productive_ppl(HashMap non_productive_ppl) {
         this.non_productive_ppl = non_productive_ppl;
     }
    
