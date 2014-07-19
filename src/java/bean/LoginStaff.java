32089a3dda4dff7d110d8a707f36e9fd5c2dcb33
qooleeman <qooleeman@gmail.com>
2014-07-19 07:16:49 +0000

    remove the duplicate attr & method

--- src/java/bean/LoginStaff.java
+++ src/java/bean/LoginStaff.java
@@ -6,72 +6,9 @@ package bean;
  */
 public class LoginStaff extends Staff{
     
-    private int staffID;
-    private String realName;
-    private String userName;
-    private UserRole role;
-    private Department  dept;
-    private String eMail;
-    private int tel;
     private String password;
 
     public LoginStaff() {
-    }
-
-    public int getStaffID() {
-        return staffID;
-    }
-
-    public void setStaffID(int staffID) {
-        this.staffID = staffID;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
-    public Department getDept() {
-        return dept;
-    }
-
-    public void setDept(Department dept) {
-        this.dept = dept;
-    }
-
-    public String geteMail() {
-        return eMail;
-    }
-
-    public void seteMail(String eMail) {
-        this.eMail = eMail;
-    }
-
-    public int getTel() {
-        return tel;
-    }
-
-    public void setTel(int tel) {
-        this.tel = tel;
     }
 
     public String getPassword() {
@@ -81,7 +18,5 @@ public class LoginStaff extends Staff{
     public void setPassword(String password) {
         this.password = password;
     }
-    
-    
-    
+        
 }
